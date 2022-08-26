124efa96554e940da237858e6516d009701f64fd
Kris Kraszewski <krzysztof.kraszewski@accounttechnologies.com>
2022-08-26 10:59:15 +0000

    Modernize

--- src/workspaceNodeProvider.ts
+++ src/workspaceNodeProvider.ts
@@ -3,12 +3,12 @@ import * as path from 'path';
 
 export interface IRunnableInfo
 {
-	Id:string;
-	Type:string;
-	DeclaredIn:string[];
-	State:RunnableState;
+    Id:string;
+    Type:string;
+    DeclaredIn:string[];
+    State:RunnableState;
     Details: { [index:string]:any };
-    
+
 }
 
 export type ServerState = 'UNKNOWN' | 'IDLE' | 'LOADED' | 'RUNNING';
@@ -16,7 +16,7 @@ export type WorkspaceState = 'UNKNOWN' | 'IDLE' | 'HEALTHY' | 'DEGRADED';
 export type RunnableState =  'ZERO' | 'INITIATED' | 'STARTED' | 'HEALTH_CHECK' | 'HEALTHY' |  'DEAD' | 'RECOVERING';
 
 export interface IWorkspaceInfo {
-	Path:string;
+    Path:string;
     Runnables:IRunnableInfo[];
     ServerState: ServerState;
     WorkspaceState:  WorkspaceState;
@@ -26,7 +26,7 @@ export class WorkspaceNode extends vscode.TreeItem {
     public children:RunnableNode[] = [];
     public id:string;
     constructor(
-                public workspaceInfo:IWorkspaceInfo, 
+                public workspaceInfo:IWorkspaceInfo,
                 protected context:vscode.ExtensionContext
                 )
     {
@@ -35,7 +35,7 @@ export class WorkspaceNode extends vscode.TreeItem {
         this.contextValue = "ring.workspace";
         this.iconPath = WorkspaceNode.getIconPath(workspaceInfo.WorkspaceState, context);
         this.children = workspaceInfo.Runnables.map(r => new RunnableNode(r, context));
-        this.id = workspaceInfo.Path;       
+        this.id = workspaceInfo.Path;
     }
 
     static getIconPath(state:WorkspaceState, context:vscode.ExtensionContext)
@@ -51,7 +51,7 @@ export class WorkspaceNode extends vscode.TreeItem {
 export class RunnableNode extends vscode.TreeItem {
 
     public id:string;
-    constructor(public runnable:IRunnableInfo, protected context:vscode.ExtensionContext) { 
+    constructor(public runnable:IRunnableInfo, protected context:vscode.ExtensionContext) {
         super(runnable.Id);
         this.runnable = runnable;
         this.label = `${runnable.Id}`;
@@ -59,28 +59,28 @@ export class RunnableNode extends vscode.TreeItem {
         this.contextValue = "ring.runnable";
         this.iconPath = RunnableNode.getIconPath(context, runnable.State);
         this.id = runnable.Id;
-     }      
+     }
 
      static getIconPath(context:vscode.ExtensionContext, State:RunnableState)
      {
-        const svg =  State === 'INITIATED' ? "initiated.svg" : 
+        const svg =  State === 'INITIATED' ? "initiated.svg" :
         State ==='STARTED' ? "started.svg" :
-        State ==='RECOVERING' ? "recovering.svg" : 
+        State ==='RECOVERING' ? "recovering.svg" :
         State ==='HEALTH_CHECK' ? "healthcheck.svg" :
         State ==='HEALTHY' ? "healthy.svg" :
         State ==='DEAD' ? "dead.svg" :
         State ==='ZERO' ? "zero.svg" : "";
         return context.asAbsolutePath(path.join("media", "status", svg));
      }
-  
+
 }
 
 export type RingNode = WorkspaceNode | RunnableNode;
 
 export class WorkspaceProvider implements vscode.TreeDataProvider<RingNode> {
-    
+
     private _onDidChangeTreeData: vscode.EventEmitter<RingNode | null> = new vscode.EventEmitter<RingNode | null>();
-	readonly onDidChangeTreeData: vscode.Event<RingNode | null> = this._onDidChangeTreeData.event;
+    readonly onDidChangeTreeData: vscode.Event<RingNode | null> = this._onDidChangeTreeData.event;
     private root:WorkspaceNode;
 
     public static emptyWorkspace:IWorkspaceInfo = {Runnables:[], Path:"Not loaded", ServerState: 'UNKNOWN', WorkspaceState: 'UNKNOWN'}
@@ -89,18 +89,17 @@ export class WorkspaceProvider implements vscode.TreeDataProvider<RingNode> {
     {
         this.root = new WorkspaceNode(WorkspaceProvider.emptyWorkspace, context);
     }
-    
+
     getTreeItem(element: RingNode): vscode.TreeItem | Thenable<vscode.TreeItem> {
         return element;
     }
 
-
     getChildren(element?: RingNode | undefined): vscode.ProviderResult<RingNode[]> {
-        
+
         if (element)
         {
             const wsNode = <WorkspaceNode>element;
-            return wsNode ? wsNode.children : [];  
+            return wsNode ? wsNode.children : [];
         }
         else
         {
@@ -109,20 +108,20 @@ export class WorkspaceProvider implements vscode.TreeDataProvider<RingNode> {
     }
 
     getRunnable(id:string) {return this.root.children.find(r => r.id === id);}
-    
+
         async updateRunnable(id:string, state: RunnableState ){
         const r = this.getRunnable(id);
         if (!r) {return;}
-        
+
         r.runnable.State = state;
         r.iconPath = RunnableNode.getIconPath(this.context, state);
-        this._onDidChangeTreeData.fire(r);       
+        this._onDidChangeTreeData.fire(r);
     }
 
     updateWorkspace(wsInfo:IWorkspaceInfo)
     {
         this.root = new WorkspaceNode(wsInfo, this.context);
-        this._onDidChangeTreeData.fire(null);       
+        this._onDidChangeTreeData.fire(null);
     }
 
     resetWorkspace()
